d9ccc22aef0c0600b0fc78e5341d70e0b91d8a4a
AFKajal <wtjuantheone@gmail.com>
2025-02-08 13:45:04 +0100

    6 Carga. Seguimos diseñando el layout

---
+++ app/src/main/java/com/example/slopshop/Administrador/MainActivityAdministrador.kt
@@ -0,0 +1,13 @@
+package com.example.slopshop.Administrador
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.slopshop.R
+
+class MainActivityAdministrador : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_administrador)
+
+        }
+}
