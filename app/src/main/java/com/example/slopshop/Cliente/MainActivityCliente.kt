6404f5c626b3c44c179e9f845b790f2050aedaed
AFKajal <wtjuantheone@gmail.com>
2025-02-08 04:12:33 +0100

    5 Carga. Añadimos logos y layout admin

---
+++ app/src/main/java/com/example/slopshop/Cliente/MainActivityCliente.kt
@@ -0,0 +1,16 @@
+package com.example.slopshop.Cliente
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.slopshop.R
+
+class MainActivityCliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_cliente)
+
+        }
+}
