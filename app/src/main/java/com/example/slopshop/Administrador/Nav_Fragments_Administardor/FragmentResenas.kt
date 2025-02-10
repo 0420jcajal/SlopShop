658fa0c32e7d4d9effb050a5a7eb1a2c88d10758
AFKajal <wtjuantheone@gmail.com>
2025-02-10 18:00:28 +0100

    7 Carga. Seguimos diseñando el layout

---
+++ app/src/main/java/com/example/slopshop/Administrador/Nav_Fragments_Administardor/FragmentResenas.kt
@@ -0,0 +1,23 @@
+package com.example.slopshop.Administrador.Nav_Fragments_Administardor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.slopshop.R
+
+
+class FragmentResenas : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_resenas, container, false)
+    }
+
+
+}
