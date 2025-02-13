bb80c73e86e3d6f11e0dc63233f05b4fd365dbe5
AFKajal <wtjuantheone@gmail.com>
2025-02-13 21:01:15 +0100

    9 Carga. Diseñamos Layour del registro admin

---
+++ app/src/main/java/com/example/slopshop/Administrador/RegistroAdministradorActivity.kt
@@ -0,0 +1,14 @@
+package com.example.slopshop.Administrador
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.slopshop.R
+
+class RegistroAdministradorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_administrador)
+
+    }
+}
