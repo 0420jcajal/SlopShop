4020e1f33154800af90e38de34f11e9981d38570
AFKajal <wtjuantheone@gmail.com>
2025-05-05 23:43:28 +0200

    29 Carga. Implementamos la carga de las categorias de la BD para que se muestren en agregar producto

--- app/src/main/java/com/example/slopshop/Administrador/Productos/AgregarProductoActivity.kt
+++ app/src/main/java/com/example/slopshop/Administrador/Productos/AgregarProductoActivity.kt
@@ -1,18 +1,25 @@
 package com.example.slopshop.Administrador.Productos
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.net.Uri
 import android.os.Bundle
+import android.renderscript.Sampler.Value
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.slopshop.Adaptador.AdaptadorImagenSeleccionada
 import com.example.slopshop.Constantes
+import com.example.slopshop.Entidades.Categoria
 import com.example.slopshop.Entidades.Imagen
 
 
 import com.example.slopshop.databinding.ActivityAgregarProductoBinding
 import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AgregarProductoActivity : AppCompatActivity() {
 
@@ -21,10 +28,14 @@ class AgregarProductoActivity : AppCompatActivity() {
 
     private lateinit var listaImagenesSeleccionadas: ArrayList<Imagen>
     private lateinit var adaptadorImagenSeleccionada: AdaptadorImagenSeleccionada
+
+    private lateinit var categoriasArrayList: ArrayList<Categoria>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityAgregarProductoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        cargarCategoría()
 
         listaImagenesSeleccionadas= ArrayList()
 
@@ -33,7 +44,45 @@ class AgregarProductoActivity : AppCompatActivity() {
             seleccionarImagen()
         }
 
+        binding.categoria.setOnClickListener{
+            seleccionarCategorias()
+        }
+
         cargarImagenes()
+    }
+    private var idCategoria= ""
+    private var nombreCategoria=""
+    private fun seleccionarCategorias(){
+        val listaCategorias= arrayOfNulls<String>(categoriasArrayList.size)
+        for(i in listaCategorias.indices){
+            listaCategorias[i]=categoriasArrayList[i].categoria
+        }
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Seleccione una categoría")
+            .setItems(listaCategorias){dialog, witch->
+                idCategoria = categoriasArrayList[witch].id
+                nombreCategoria = categoriasArrayList[witch].categoria
+                binding.categoria.text= nombreCategoria
+            }
+            .show()
+    }
+
+    private fun cargarCategoría() {
+        categoriasArrayList= ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Categorías").orderByChild("categoria")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot){
+                categoriasArrayList.clear()
+                for (ds in snapshot.children){
+                    val modelo = ds.getValue(Categoria::class.java)
+                    categoriasArrayList.add(modelo!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //TODO LOG ERROR
+            }
+        })
     }
 
     private fun cargarImagenes() {
---
+++ app/src/main/java/com/example/slopshop/Entidades/Categoria.kt
@@ -0,0 +1,13 @@
+package com.example.slopshop.Entidades
+
+class Categoria {
+
+    var id : String = ""
+    var categoria : String = ""
+
+    constructor()
+    constructor(id: String,categoria: String) {
+        this.id = id
+        this.categoria=categoria
+    }
+}
